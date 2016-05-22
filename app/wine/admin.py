94b4456bfc9ba663fd2765c8d3c3ff67d4838458
Chris Barna <chris@unbrain.net>
2016-05-22 14:37:41 -0400

    Ch-ch-changes!

--- app/wine/admin.py
+++ app/wine/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Wine, Grape, Winery
+
+class GrapeInline(admin.TabularInline):
+    model = Grape
+
+
+@admin.register(Wine)
+class WineAdmin(admin.ModelAdmin):
+    fieldsets = (
+        ('Bottle', {
+            'fields': ('bottle_text', 'year', 'winery',),
+            }),
+        ('Purchase', {
+            'fields': ('date_purchased', 'store', 'importer'),
+            }),
+        ('Consumption', {
+            'fields': ('date_consumed', 'liked_it', 'notes',),
+            })
+    )
+
+    inlines = [
+        GrapeInline,
+    ]
+
+
+admin.site.register(Winery)
--- app/wine/models.py
+++ app/wine/models.py
@@ -2,4 +2,34 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Wine(models.Model):
+    bottle_text = models.CharField(max_length=100)
+    year = models.IntegerField()
+
+    date_purchased = models.DateField()
+
+    # A few more of these could be foreign keys. Keeping the data model simple
+    # but might change because of wine laziness.
+    store = models.CharField(max_length=50)
+    winery = models.ForeignKey('Winery')
+    importer = models.CharField(max_length=50)
+
+    date_consumed = models.DateField(blank=True, null=True)
+    liked_it = models.NullBooleanField(blank=True, null=True)
+    notes = models.TextField(blank=True, default="")
+
+
+class Grape(models.Model):
+    wine = models.ForeignKey(Wine)
+    name = models.CharField(max_length=50)
+    percentage = models.IntegerField(default=100)
+
+
+class Winery(models.Model):
+    name = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
+    region = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name_plural = "wineries"
