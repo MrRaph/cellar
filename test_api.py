d7d47a16f91f3d73d51bbab7d6e7d6f37397fb54
MrRaph_ <raphael.charrat@aldanet.fr>
2016-08-02 13:10:38 +0200

    Delete test_api.py

--- test_api.py
+++
@@ -1,6 +0,0 @@
-from outpan import OutpanApi
-api = OutpanApi('e192a3e203c169df6edcfd9cd5f09300')
-print(api.get_product("3245414984219"))
-
-
-http://services.wine.com/api/beta2/service.svc/format/resource?apikey=4139289e8d61ee89fcd5e2a14a9220f8&parameters
